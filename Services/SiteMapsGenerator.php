060882adbcde7daebb7851b0438696f96d21653d
Motch Julien <ju.motch@gmail.com>
2016-06-12 19:05:33 +0200

    Check module
    Check is the module is present and enable

--- Services/SiteMapsGenerator.php
+++ Services/SiteMapsGenerator.php
@@ -3,123 +3,132 @@
 namespace Modules\SiteMap\Services;
 
 use Illuminate\Contracts\Foundation\Application;
-use Modules\Page\Entities\Page;
-use Modules\Blog\Entities\Post;
-use LaravelLocalization;
+    use Modules\Workshop\Manager\ModuleManager;
+    use LaravelLocalization;
 
-class SiteMapsGenerator
-{
-    /**
-     * @var Application
-     */
-    private $app;
-
-    /**
-     * @var PageRepository
-     */
-    private $page;
-
-    /**
-     * @var PostRepository
-     */
-    private $post;
-
-    /**
-     * @var SiteMaps
-     */
-    private $sitemap;
-
-    public function __construct(Application $app, Page $page, Post $post)
+    class SiteMapsGenerator
     {
-        $this->app = $app;
-        $this->page = $page;
-        $this->post = $post;
-        $this->sitemap = $this->app->make('sitemap');
-        $this->availableLanguage = $this->getAllLanguageAvailable();
-    }
+        /**
+         * @var Application
+         */
+        private $app;
 
-    public function generateSiteMaps()
-    {
-        $this->itemSiteMaps();
+        private $moduleManager;
 
-        $this->sitemap->store('xml', 'mysitemap');
+        /**
+         * @var SiteMaps
+         */
+        private $sitemap;
 
-        return $this->sitemap->render('xml');
-    }
+        public function __construct(Application $app, ModuleManager $moduleManager)
+        {
+            $this->app = $app;
+            $this->sitemap = $this->app->make('sitemap');
+            $this->availableLanguage = $this->getAllLanguageAvailable();
+            $this->moduleManager = $moduleManager->enabled();
+        }
 
-    private function itemSiteMaps()
-    {
-        // For Pages
-        $this->allPages();
+        public function generateSiteMaps()
+        {
+            $this->itemSiteMaps();
 
-        // For Blog, all Posts
-        $this->allPosts();
-    }
+            $this->sitemap->store('xml', 'mysitemap');
 
-    private function getTranslation($collection, $route, $priority, $frequency)
-    {
-        $allLanguage = $this->availableLanguage;
-        $firstLanguage = $this->availableLanguage[0];
-        unset($allLanguage[0]);
+            return $this->sitemap->render('xml');
+        }
 
-        $defaultPriority = $priority;
-        $defaultFrequency = $frequency;
+        private function itemSiteMaps()
+        {
+            // For Pages
+            $this->allPages();
 
-        foreach ($collection as $item) {
-            $translations = [];
+            // For Blog, all Posts
+            $this->allPosts();
+        }
 
-            if (count($allLanguage)) {
-                foreach ($allLanguage as $key => $language) {
-                    $translations[] = ['language' => $language, 'url' => $this->getRouteParams($item->translate($language), $route, $language)];
+        private function getTranslation($collection, $route, $priority, $frequency)
+        {
+            $allLanguage = $this->availableLanguage;
+            $firstLanguage = $this->availableLanguage[0];
+            unset($allLanguage[0]);
+
+            $defaultPriority = $priority;
+            $defaultFrequency = $frequency;
+
+            foreach ($collection as $item) {
+                $translations = [];
+
+                if (count($allLanguage)) {
+                    foreach ($allLanguage as $key => $language) {
+                        $translations[] = ['language' => $language, 'url' => $this->getRouteParams($item->translate($language), $route, $language)];
+                    }
                 }
+
+                /* If home page, else default Priority */
+                $priority = isset($item->is_home) && $item->is_home === 1 ? '1.0' : $defaultPriority;
+                $frequency = isset($item->is_home) && $item->is_home === 1 ? 'weekly' : $defaultFrequency;
+
+                $this->sitemap->add($this->getRouteParams($item->translate($firstLanguage), $route, $firstLanguage), $item->updated_at, $priority, $frequency, [], null, $translations);
+            }
+        }
+
+        private function getRouteParams($item, $route, $language)
+        {
+            $params = [];
+
+            foreach ($route['params'] as $key => $value) {
+                $params = array_merge($params, ["$key" => $item->{$value}]);
             }
 
-            /* If home page, else default Priority */
-            $priority = isset($item->is_home) && $item->is_home === 1 ? '1.0' : $defaultPriority;
-            $frequency = isset($item->is_home) && $item->is_home === 1 ? 'weekly' : $defaultFrequency;
+            $routeParams = $route['route'];
+            $locale = locale();
 
-            $this->sitemap->add($this->getRouteParams($item->translate($firstLanguage), $route, $firstLanguage), $item->updated_at, $priority, $frequency, [], null, $translations);
-        }
-    }
+            if (array_key_exists('locale', $route)) {
+                $routeParams = $locale.'.'.$routeParams;
+            }
 
-    private function getRouteParams($item, $route, $language)
-    {
-        $params = [];
+            if ($locale !== $language) {
+                $defaultUrl = route($routeParams, $params);
 
-        foreach ($route['params'] as $key => $value) {
-            $params = array_merge($params, ["$key" => $item->{$value}]);
-        }
+                return LaravelLocalization::getLocalizedURL($language, $defaultUrl);
+            }
 
-        $routeParams = $route['route'];
-        $locale = locale();
-
-        if (array_key_exists('locale', $route)) {
-            $routeParams = $locale.'.'.$routeParams;
+            return route($routeParams, $params);
         }
 
-        if ($locale !== $language) {
-            $defaultUrl = route($routeParams, $params);
-
-            return LaravelLocalization::getLocalizedURL($language, $defaultUrl);
+        private function getAllLanguageAvailable()
+        {
+            return LaravelLocalization::getSupportedLanguagesKeys();
         }
 
-        return route($routeParams, $params);
-    }
+        private function allPages()
+        {
+            $value = ['name' => 'Page', 'entities' => 'Modules\Page\Entities\Page'];
+            $query = $this->moduleIsPresentAndEnable($value);
 
-    private function getAllLanguageAvailable()
-    {
-        return LaravelLocalization::getSupportedLanguagesKeys();
-    }
+            if ($query) {
+                $collection = $query->translated()->get();
+                $this->getTranslation($collection, ['route' => 'page', 'params' => ['uri' => 'slug']], '0.8', 'monthly');
+            }
+        }
 
-    private function allPages()
-    {
-        $collection = $this->page->translated()->get();
-        $this->getTranslation($collection, ['route' => 'page', 'params' => ['uri' => 'slug']], '0.8', 'monthly');
-    }
+        private function allPosts()
+        {
+            $value = ['name' => 'Blog', 'entities' => 'Modules\Page\Entities\Blog'];
+            $query = $this->moduleIsPresentAndEnable($value);
 
-    private function allPosts()
-    {
-        $collection = $this->post->translated()->where('status', 2)->get();
-        $this->getTranslation($collection, ['route' => 'blog.slug', 'locale' => true, 'params' => ['uri' => 'slug']], '0.6', 'monthly');
+            if ($query) {
+                $collection = $this->post->translated()->where('status', 2)->get();
+                $this->getTranslation($collection, ['route' => 'blog.slug', 'locale' => true, 'params' => ['uri' => 'slug']], '0.6', 'monthly');
+            }
+        }
+
+        private function moduleIsPresentAndEnable(array $value)
+        {
+            if (array_key_exists($value['name'], $this->moduleManager)) {
+                return $this->app->make($value['entities']);
+            }
+
+            return false;
+        }
     }
-}
